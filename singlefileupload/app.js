9c12cbb85990ebc72db5ee5d52fed7abaf686407
enoch232 <enoch232@gmail.com>
2016-07-01 15:39:46 -0700

    renaming for the uploads

--- singlefileupload/app.js
+++ singlefileupload/app.js
@@ -1,6 +1,14 @@
 var express = require("express");
 var multer = require("multer");
-var upload = multer({dest: 'uploads/'});
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now())
+  }
+});
+var upload = multer({storage: storage});
 var bodyParser = require("body-parser");
 var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
